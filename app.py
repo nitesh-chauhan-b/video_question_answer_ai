f90c07df71bacbb0443460fd1f58c534ed95c9bd
nitesh-chauhan-b <nkchauhan9090@gmail.com>
2025-04-29 15:10:09 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import tempfile
+import os
+import helper
+#For removing the error
+os.environ["STREAMLIT_DISABLE_WATCHDOG_WARNING"] = "true"
+
+
+st.set_page_config(layout="wide")  # Enable wide layout
+st.title("🎥 Question Answer With Video")
+
+# Upload video
+uploaded_file = st.file_uploader("Upload a Video File", type=["mp4", "mov", "avi", "mkv"])
+
+if uploaded_file is not None:
+    # Save video to a temporary file
+    tfile = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
+    tfile.write(uploaded_file.read())
+
+    # Get video path
+    video_path = tfile.name
+
+
+
+    # Layout: Two columns
+    col1, col2 = st.columns([2, 2])  # 2:1 width ratio
+
+    # Column 1: Video preview
+    with col1:
+        st.video(video_path)
+        st.success(f"Uploaded: {uploaded_file.name}")
+
+    # Column 2: Q&A section
+    with col2:
+
+        #Getting video ready for question and answers
+        try:
+            # Getting transcription from the video
+            video_transcript = helper.get_video_content(video_path)
+            # print(video_transcript)
+
+            # Storing the data
+            helper.store_data(video_transcript)
+
+            # Getting Question answer chain
+            chain = helper.get_qa_chain()
+
+            print("Success!!")
+
+            st.header("Question & Answer")
+            query = st.text_input("Enter Your Question")
+            submit = st.button("Submit")
+
+            if query and submit:
+
+                #Getting the answer
+                response = chain(query)
+
+                st.header("Answer")
+
+                st.markdown(response["result"],unsafe_allow_html=True)
+
+        except Exception as e:
+            print("Error : ", e)
+
+
---
+++ helper.py
@@ -0,0 +1,146 @@
+import os.path
+
+from dotenv import load_dotenv
+load_dotenv()
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+#Loading the model it it doesn't require the api key
+
+# import whisper
+#
+# model = whisper.load_model("turbo")
+
+#Using faster whisper model
+from faster_whisper import WhisperModel
+
+#Loading model
+model = WhisperModel(
+    "base",
+    device="cpu",
+    compute_type="int8",
+
+)
+
+#Embeddings for vector db
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+
+llm = ChatGroq(
+    model='llama-3.3-70b-versatile',
+    # temperature=0.6,
+    max_tokens=1000
+)
+
+
+def get_video_content(path):
+    # Original Whisper model
+
+    # result = model.transcribe(path,verbose=True)
+    # # Getting the results from the file
+    # video_transcription = result["text"]
+    # # print(result["text"])
+    #
+    # return video_transcription
+
+    #Loading faster whisper model
+
+    #It uses the segments and to divide video into segments
+    segments,info = model.transcribe(
+        path,
+        beam_size=1,
+        word_timestamps=False
+    )
+
+    #Combining segments to get the transcribes test
+    video_transcript = "".join([seg.text for seg in segments])
+
+    return video_transcript
+
+def store_data(video_transcription):
+
+    splitter = RecursiveCharacterTextSplitter(
+        separators=[".", "\n", "\n\n"],
+        chunk_size=1000,
+        chunk_overlap=100
+    )
+
+    chunks = splitter.split_text(video_transcription)
+
+    db = Chroma.from_texts(chunks, embedding, collection_name="video_transcription", persist_directory="./chroma_db")
+
+def get_qa_chain():
+    if os.path.exists("chroma_db"):
+        db = Chroma(persist_directory="./chroma_db", embedding_function=embedding)
+
+    #Getting db as retiver
+    retriever = db.as_retriever()
+
+    # Creating a template for getting answers
+    # template = """Given the following context and a question, generate an answer based on this context only.
+    #     In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
+    #     If context doesn't contain answer then answer in short form about i don't know.
+    #     Try to give examples to explain the question better.
+    #
+    #     CONTEXT: {context}
+    #
+    #     QUESTION: {question}
+    # """
+
+    template = """Given the question, provide a detailed, easy-to-understand answer based on the content in the video.
+        The answer should be as direct and informative as possible without referencing the specific context.
+        If an example is provided, format it clearly to make it easier to follow.
+        If the content doesn’t answer the question directly, provide a concise and polite response indicating that the answer is unknown.
+        
+        Content: {context}
+        
+        QUESTION: {question}
+
+        Ensure the response is clear, with simple explanations and structured examples if necessary. Avoid making unnecessary references to the source material directly.
+    """
+
+    prompt = PromptTemplate(
+        input_variables=["context", "question"],
+        template=template
+    )
+
+
+
+    # Creating a question answer chain
+    chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        input_key="query",
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt}
+    )
+
+    return chain
+
+
+if __name__ == "__main__":
+
+    video_path= "resource/python_string_method.mp4"
+
+    try:
+        print("Converting video into text....")
+        video_transcript = get_video_content(video_path)
+
+        print("Storing the data...")
+        store_data(video_transcript)
+
+        print("Creating the QA Chain")
+        chain = get_qa_chain()
+
+        query = "What is string?"
+
+        response = chain(query)
+
+        print("\n\nAnswer :\n",response["result"])
+    except Exception as e:
+        print("Something went wrong : ",e)
